a0bfa68465a04c5b15ade0cb0ceb1924fcbbab30
Zinc <minkevicius.linas@gmail.com>
2019-01-22 18:02:22 +0200

    Create states.h

---
+++ Core/Inc/states.h
@@ -0,0 +1,15 @@
+/* USER CODE BEGIN Includes */
+/*
+ * states.h
+ *
+ *  Created on: Jan 22, 2019
+ *      Author: Caustic_Zinc
+ */
+
+#ifndef INC_STATES_H_
+#define INC_STATES_H_
+
+
+
+#endif /* INC_STATES_H_ */
+/* USER CODE END Includes */
